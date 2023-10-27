31bd48e9dff1447adb27500d5e5b2dbd0a138ee0
Florin Hasna <florinhasna@outlook.com>
2023-10-27 20:58:29 +0100

    Sum of elements in range
    Wrote a program that ask 2 numbers from the user, and computes the sum of all numbers in the range.

--- sumrange.cpp
+++ sumrange.cpp
@@ -2,5 +2,21 @@
 using namespace std;
 
 int main(){
-  
+  // declare two integers
+  int num1, num2;
+  // prompt user
+  std::cout << "Enter two numbers: ";
+  // read input
+  std::cin >> num1 >> num2;
+
+  // declare and initialise a variable to store result
+  int result = 0;
+
+  // iterates all the value in the range num1 - num2 and adds it to result
+  for (int i = num1; i <= num2; i++)
+    result = result + i;
+
+  // print result
+  std::cout << "The sum from " << num1 << " to " << num2;
+  std::cout << " is " << result << endl;
 }
