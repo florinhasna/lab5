fbab3f747c39b23f83b71e51b0292223673d97c4
Florin Hasna <florinhasna@outlook.com>
2023-10-28 20:50:44 +0100

    Update:
    - Created double variable to add grade input with every iteration.
    - Added OR in the if statement to evaluate true when number of iterations reaches 101, therefore 100 grades were already stored in the vector.
    - In the same if statement, the average is computed.
    - Added if statement outside the loop to check if any input was given, where the average is printed if yes.

--- gradestats.cpp
+++ gradestats.cpp
@@ -3,26 +3,48 @@
 
 int main(){
   // initialize an integer to store grade input
-  int grade;
+  double grade;
   
   // initialize a vector
   std::vector<int> gradesVector;
 
+  // declare a result variable
+  double averageResult = 0;
+
   // loop to read user data 100 times
-  for (int i = 0; i < 100; i++){
+  for (int i = 0; i <= 100; i++){
     std::cout << "Enter grade (or -1 to end): "; 
     std::cin >> grade;
 
-    // end the loop if the user chose so
-    if (grade == -1)
-      break;
+    // end the loop if the user chose so or already 100 grades stored
+    if (grade == -1 || i == 100){
+      // get the number of grades in vector
+      int numberOfGrades = gradesVector.size();
 
+      // calculate average
+      averageResult = averageResult / numberOfGrades;
+
+      // stop looping
+      break;
+    }
     // increase vector length by 1
     gradesVector.push_back(i + 1);
 
     // add the grade to the vector
     gradesVector[i] = grade;
+
+    // add the grade to the averageResult
+    averageResult += grade;
   }
+
+  // print result
+  if (averageResult > 0)
+    std::cout << "The average of the grades is: " << averageResult << std::endl;
+
+  // no grade input
+  else
+    std::cout << "No grades entered." << std::endl;
+
   
   return 0; 
 }
