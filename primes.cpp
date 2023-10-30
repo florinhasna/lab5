dbed9d9aa54bd7f96ab8423e182f36795387c5ad
Florin Hasna <florinhasna@outlook.com>
2023-10-30 13:17:22 +0000

    The finalised program reads a number from user, and prints all prime numbers to the given number.

--- primes.cpp
+++ primes.cpp
@@ -1,12 +1,30 @@
 #include <iostream>
 
 int main(){
+  // get user input
   int userInput;
   std::cout << "Enter an integer: ";
   std::cin >> userInput;
 
-  for (int i = 0; i <= userInput; i++){
-    std::cout << "Primes to " << userInput << " are: ";
+  // used to pass on if a numbers is prime
+  bool isPrime = false;
+  std::cout << "Primes to " << userInput << " are: 2 ";
+  // from i to userInput inclusive, test all numbers
+  for (int i = 3; i <= userInput; i++){
+
+    // check if is prime
+    for(int j = 2; j < i; j++)
+      if (i % j != 0)
+	isPrime = true;
+    // if a divisor found, break loop
+      else {
+	isPrime = false;
+	break;
+      }
+
+     // if i is prime, print.
+     if (isPrime)
+	std::cout << i << " ";
   }
     
   return 0;
