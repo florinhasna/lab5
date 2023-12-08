7756d7755435555c7c1424581cc3a241cf9b03e0
Florin Hasna <florinhasna@outlook.com>
2023-12-08 09:50:36 +0000

    Program displays minimum and maximum of the array

--- MinAndMax.cpp
+++ MinAndMax.cpp
@@ -2,16 +2,30 @@
 using namespace std;
 
 int main(){
-  int desiredNumbers = 10;
+  const int DESIRED_NUMBERS = 10;
   // create an array to store generated numbers
   int randomNumbers[10];
+  int min = 100;
+  int max = 0;
 
   // loop to generate 10 numbers and add to the vector
-  for (int i = 0; i < desiredNumbers; i++){
+  cout << "The elements are: ";
+  for (int i = 0; i < DESIRED_NUMBERS; i++){
     int generatedNumber = rand() % (100 + 1 - 0) + 0;
     randomNumbers[i] = generatedNumber;
-    cout << randomNumbers[i] << endl;
+
+    if(randomNumbers[i] < min){
+      min = randomNumbers[i];
+    }
+    if (randomNumbers[i] > max){
+      max = randomNumbers[i];
+    }
+
+    cout << randomNumbers[i] << ", ";
   }
+
+  cout << "\n" << "The minimum is: " << min << endl;
+  cout << "The maximum is: " << max << endl;
   
   return 0;
 }
